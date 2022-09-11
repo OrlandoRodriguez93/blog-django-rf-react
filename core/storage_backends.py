88ea90d8e023c4de654ae39d2b753d1bd4c8f886
jlrodriguezl <orlando.rodriguez93@hotmail.com>
2022-09-11 15:52:11 -0500

    Commit inicial, configuracion realizada

---
+++ core/storage_backends.py
@@ -0,0 +1,13 @@
+"""
+Permite el correcto funcionamiento de S3
+"""
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticsStorage(S3Boto3Storage):
+    location = 'static'
+    default_acl = 'private'
+
+class MediaStore(S3Boto3Storage):
+    location = 'static'
+    file_overwrite = False
